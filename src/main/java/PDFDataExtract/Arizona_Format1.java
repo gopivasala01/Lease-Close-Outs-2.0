e31b6c87e1385d357afc843013a545e39113ab97
gopivasala01 <gopi.v@beetlerim.com>
2024-01-25 00:32:05 -0500

    Removed TestData In Format1

--- src/main/java/PDFDataExtract/Arizona_Format1.java
+++ src/main/java/PDFDataExtract/Arizona_Format1.java
@@ -207,10 +207,10 @@ public class Arizona_Format1
 	    		  PDFReader.increasedRent_amount = increasedRentRaw.substring(increasedRentRaw.indexOf("shall be $")+"shall be $".length()).trim().split(" ")[0];
 	    		  System.out.println("Increased Rent - Amount = "+PDFReader.increasedRent_amount); 
 	    	}
-	    	else
+	    	/* else
 	    	{
 	    		TestDataExtraction.extractIncreasedRentDetails(text);
-	    	}
+	    	}*/
 	    }
 	    catch(Exception e)
 	    {
--- src/main/java/PDFDataExtract/DFW_Format1.java
+++ src/main/java/PDFDataExtract/DFW_Format1.java
@@ -153,10 +153,10 @@ public class DFW_Format1
 	    		  PDFReader.increasedRent_amount = increasedRentRaw.substring(increasedRentRaw.indexOf("shall be $")+"shall be $".length()).trim().split(" ")[0];
 	    		  System.out.println("Increased Rent - Amount = "+PDFReader.increasedRent_amount); 
 	    	}
-	    	else
+	    	/* else
 	    	{
 	    		TestDataExtraction.extractIncreasedRentDetails(text);
-	    	}
+	    	}*/
 	    }
 	    catch(Exception e)
 	    {
