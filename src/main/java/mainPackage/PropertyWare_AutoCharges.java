a8f62f092f7bef3eddae8f6eb43b4ab6b0f05132
Azure SQL DB <service.sql01@homeriver.com>
2024-01-26 08:08:14 -0500

    Auto Charges code enabled

--- src/main/java/mainPackage/PropertyWare_AutoCharges.java
+++ src/main/java/mainPackage/PropertyWare_AutoCharges.java
@@ -45,7 +45,7 @@ public class PropertyWare_AutoCharges
 					RunnerClass.statusID=1;
 					continue;
 				}
-				/*
+				
 				try
 				{
 				List<WebElement> existingAutoCharges = RunnerClass.driver.findElements(Locators.autoCharge_List);
@@ -68,7 +68,7 @@ public class PropertyWare_AutoCharges
 						amount = amount.substring(0,amount.indexOf("."));
 					}
 					catch(Exception e)
-					{} // Here add removed comment close
+					{} */// Here add removed comment close
 					if(chargeCode.contains(autoChargeCodes.replaceAll(".", ""))&&autoChargeAmount.replaceAll("[^0-9]", "").equals(amount.replaceAll("[^0-9]", ""))||amount.trim().equals("0.00"))//&&(startDate.equals(autoChargeStartDate)||autoChargeEndDate.trim().equals("")))
 					{
 						availabilityCheck = true;
@@ -79,7 +79,7 @@ public class PropertyWare_AutoCharges
 				}
 				catch(Exception e)
 				{}
-	      */
+	      
 				if(availabilityCheck==false)
 				{
 					PropertyWare_AutoCharges.addingAnAutoCharge(chargeCode, amount, startDate,endDate, description);
